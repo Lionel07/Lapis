222928edfbabf562da79c009e79e6669aa0b2e7c
Corwin McKnight <lioneltabre@gmail.com>
2015-10-12 02:53:21 -0700

    Added GRUB_BOOT compiler option

--- arch/x86/i386/early_main.cpp
+++ arch/x86/i386/early_main.cpp
@@ -5,7 +5,11 @@
 #include <arch/x86/gdt.h>
 #include <arch/x86/idt.h>
 void kmain();
+#ifdef GRUB_BOOT
+extern "C" void early_kernel_main(int magic, void* bootinfo) {
+#else
 extern "C" void early_kernel_main() {
+#endif
 	TextConsole::Init();
 	Kernel::Version::printKernelVersion();
 	printk(LOG_NOTICE,"\n");
